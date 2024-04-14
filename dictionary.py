6da111d260800b71276014760ce63f4589c87fa3
Arman11980 <165942302+Arman11980@users.noreply.github.com>
2024-04-14 21:21:04 +0300

    Add files via upload

--- dictionary.py
+++ dictionary.py
@@ -1,13 +1,13 @@
 my_list = ['coconud', 'apple', 'kivi','pear','mango']
-first_list = my_list[0]
-last_list = my_list[-1]
-print(first_list)
-print(last_list)
+print(my_list)
+print(my_list[0])
+print(my_list[-1])
+print(my_list[2:5])
 my_list[2] = 'peach'
 print(my_list)
 
 my_dict = {'cocound': 'кокос', 'apple': 'яблоко', 'pear': 'груша', 'mango': 'манго'}
 print(my_dict)
 print(my_dict['cocound'])
-my_dict.update({'peach': 'персик'})
+my_dict['peach']= 'персик'
 print(my_dict)
